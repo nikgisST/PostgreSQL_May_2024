6bddcf21b10074f17271a235ef8367f9a7f03cf3
nikgisST <149585913+nikgisST@users.noreply.github.com>
2024-08-15 17:24:18 +0300

    exercises

--- 18 - Exam Preparation/SQLquerySolutions/2.4_delete.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM clients AS cl
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM courses AS cou
-    WHERE cou.client_id = cl."id"
-)
-AND LENGTH(cl.full_name) > 3;
--- 18 - Exam Preparation/SQLquerySolutions/3.5_cars.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	c.make, 
-	c.model, 
-	c."condition"
-FROM 
-	cars AS c
-ORDER BY 
-	c."id";
